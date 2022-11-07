fed05ecbd21596c64ee4e89d7a94ddcffc338465
Danny Bañol <alphagrim90@gmail.com>
2022-11-07 12:11:14 -0500

    updated to use with expressjs

--- app/server.js
+++ app/server.js
@@ -1,13 +1,31 @@
+import path from 'path';
+import { fileURLToPath } from 'url';
+import express from 'express';
 import { Server } from 'socket.io';
+import { createServer } from 'http';
 
 
-const io = new Server(3000, {
+const __dirname = path.resolve();
+const __filename = fileURLToPath(import.meta.url);
+
+const app = express();
+const httpServer = createServer(app);
+const io = new Server(httpServer, {
     cors: {
         origin: '*',
     },
 });
+const port = 3000;
 
 let appUsers = {};
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', (req, res) => {
+    const index = path.join(__dirname, 'index.html');
+    res.sendFile(index);
+});
+
 
 io.on('connection', (socket) => {
 
@@ -23,4 +41,21 @@ io.on('connection', (socket) => {
         delete appUsers[socket.id];
     })
 
+});
+
+httpServer.listen(port, () => {
+    // get dirname
+
+    console.log(`Listening on port ${port}`);
+    console.log(`__dirname: ${__dirname}`);
+    console.log(`__filename: ${__filename}`);
+    // console.log(`process.cwd(): ${process.cwd()}`);
+    // console.log(`process.argv: ${process.argv}`);
+    // console.log(`process.execPath: ${process.execPath}`);
+    // console.log(`process.execArgv: ${process.execArgv}`);
+    // console.log(`process.env: ${process.env}`);
+    // console.log(`process.version: ${process.version}`);
+    // console.log(`process.versions: ${process.versions}`);
+    // console.log(`process.config: ${process.config}`);
+    // console.log(`process.pid: ${process.pid}`);
 });
